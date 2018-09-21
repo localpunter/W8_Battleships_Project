c7f45069927a331538b40133ba15f95d80430e32
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-22 00:22:00 +0100

    Add info for player 2 in grid_view

--- battleships_game/client/src/views/grid_view.js
+++ battleships_game/client/src/views/grid_view.js
@@ -5,10 +5,16 @@ const createAndAppend = require('../helpers/create_append.js');
 const GridView = function (container, gamestate) {
   this.container = container;
   this.gamestate = gamestate;
+  //we might need to pass number of guesses
 };
 
 GridView.prototype.render = function (event) {
-  const gridTable = createAndAppend('table', 'grid-table', '', this.container)
+
+  //info
+  const info = `Unleash your wrath player 2, it's time to sink the enemy fleet. A simple click on the grid will be enough, but be sure to aim well, you only have ${this.guessesLeft} guesses. You have to sink 5 ships in total.`
+  createAndAppend('h3', 'player-2-info', info, this.container)
+
+  const gridTable = createAndAppend('table', 'grid-table', '', this.container);
   console.log(this.gamestate);
   for (let i = 0; i < this.gamestate.length; i++) {
     const row = createAndAppend('tr', null , '', gridTable);
