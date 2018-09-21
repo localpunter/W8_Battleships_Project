053d30f944d6f7980663019e64f200c7458a8edc
Simon Shephard <simonshephard@hotmail.com>
2018-09-21 22:09:43 +0100

    adds table to grid view

--- battleships_game/client/src/views/grid_view.js
+++ battleships_game/client/src/views/grid_view.js
@@ -1,5 +1,6 @@
 const PubSub = require('../helpers/pub_sub.js');
 const GridTileView = require('./grid_tile_view.js');
+const createAndAppend = require('../helpers/create_append.js');
 
 const GridView = function (container, gamestate) {
   this.container = container;
@@ -7,11 +8,12 @@ const GridView = function (container, gamestate) {
 };
 
 GridView.prototype.render = function (event) {
-  const gridDiv = document.createElement('div');
-  this.container.appendChild(gridDiv);
+  const gridTable = createAndAppend('table', 'grid-table', '', this.container)
+  console.log(this.gamestate);
   for (let i = 0; i < this.gamestate.length; i++) {
+    const row = createAndAppend('tr', null , '', gridTable);
     for (let j = 0; j < this.gamestate[0].length; j++) {
-      const gameTileView = new GridTileView(gridDiv, i.toString() + j.toString(), this.gamestate[i][j]);
+      const gameTileView = new GridTileView(row, i.toString() + j.toString(), this.gamestate[i][j]);
       gameTileView.bindEvents();
     }
   }
