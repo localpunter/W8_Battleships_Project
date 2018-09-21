4aac337c075c4cc8a22b6b93a7e1ae9dda3e55d2
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-22 00:02:55 +0100

    Add battleships info for the form

--- battleships_game/client/src/views/form_view.js
+++ battleships_game/client/src/views/form_view.js
@@ -11,12 +11,16 @@ const FormView = function (container, gamestate) {
 
 FormView.prototype.render = function (event) {
   console.log('form view rendering');
+  // info
+  
+  const info = "Welcome to Battleship player 1, your mission is to hide your ships well in this grid so that you can emerge victorious from the rage of player 2. You have 5 ships to place and to do it you just have to click at the tiles"
 
+  createAndAppend('h3', 'game-info', info , this.container)
   const formTable = createAndAppend('table', 'form-table', '', this.container)
   console.log(this.gamestate);
   for (let i = 0; i < this.gamestate.length; i++) {
     const row = createAndAppend('tr', null , '', formTable)
-    for (let j = 0; j < this.gamestate[0].length; j++) {      
+    for (let j = 0; j < this.gamestate[0].length; j++) {
       const formTileView = new FormTileView(row, i.toString() + j.toString(), this.gamestate[i][j]);
       formTileView.bindEvents();
     }
