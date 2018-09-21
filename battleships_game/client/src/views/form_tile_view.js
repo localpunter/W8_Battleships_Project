10af0671d626611f2cf1415a83d20f4c0861b2bc
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-21 20:17:06 +0100

    Add id to each of the tiles

--- battleships_game/client/src/views/form_tile_view.js
+++ battleships_game/client/src/views/form_tile_view.js
@@ -1,13 +1,15 @@
 const PubSub = require('../helpers/pub_sub.js');
+const createAndAppend = require('../helpers/create_append.js');
 
 const FormTileView = function (container, id) {
   this.container = container;
   this.id = id;
+  this.status = 'Empty';
+  // pass status from form_view and the model
 };
 
 FormTileView.prototype.bindEvents = function () {
-  const formTile = document.createElement('div');
-  this.container.appendChild(formTile);
+  const formTile = createAndAppend('td', this.id, this.status, this.container);
   formTile.addEventListener('click', (event) => {
     this.handleClick(event);
   });
