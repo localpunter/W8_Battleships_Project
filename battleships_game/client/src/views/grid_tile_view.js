384dc6a786cb242de185d8c00587e823706806e7
Simon Shephard <simonshephard@hotmail.com>
2018-09-21 22:01:27 +0100

    adds status parameter to grid tile view

--- battleships_game/client/src/views/grid_tile_view.js
+++ battleships_game/client/src/views/grid_tile_view.js
@@ -1,13 +1,17 @@
 const PubSub = require('../helpers/pub_sub.js');
+const createAndAppend = require('../helpers/create_append.js');
 
-const GridTileView = function (container, id) {
+const GridTileView = function (container, id, status) {
   this.container = container;
   this.id = id;
+  this.status = status;
+  // pass status from grid_view and the model
 };
 
 GridTileView.prototype.bindEvents = function () {
-  const gridTile = document.createElement('div');
-  this.container.appendChild(gridTile);
+  this.handleStatus()
+  console.log('status:', this.status);
+  const gridTile = createAndAppend('td', this.id, this.status, this.container);
   gridTile.addEventListener('click', (event) => {
     this.handleClick(event);
   });
@@ -17,5 +21,16 @@ GridTileView.prototype.handleClick = function (event) {
   PubSub.publish('GridTileView:tile-clicked', event);  //pass id here event.target.id??
 };
 
+GridTileView.prototype.handleStatus = function () {
+  if (this.status === 0) {
+    this.status = "Empty"
+  } else if (this.status === 1) {
+    this.status = "MISS"
+  } else if (this.status === 2) {
+    this.status = "Ship"
+  } else if (this.status === 3) {
+    this.status = "HIT"
+  }
+};
 
 module.exports = GridTileView;
