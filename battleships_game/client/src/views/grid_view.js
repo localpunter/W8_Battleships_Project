b71403c2abe6afe0c2e04d609d2eac69d03c810b
Alan Hunter <localpunter@sky.com>
2018-09-22 13:27:11 +0100

    css files added

--- battleships_game/client/src/views/grid_view.js
+++ battleships_game/client/src/views/grid_view.js
@@ -12,7 +12,8 @@ const GridView = function (container, gamestate, attemptsLeft) {
 GridView.prototype.render = function (event) {
 
   //info
-  const info = `Unleash your wrath player 2, it's time to sink the enemy fleet. A simple click on the grid will be enough, but be sure to aim well, you only have ${this.attemptsLeft} guesses. You have to sink 5 ships in total.`
+  const info = `Unleash your wrath player 2, it's time to sink the enemy fleet.
+  A simple click on the grid will be enough, but be sure to aim well, you only have ${this.attemptsLeft} guesses. You have to sink 5 ships in total.`
   createAndAppend('h3', 'player-2-info', info, this.container)
 
   const gridTable = createAndAppend('table', 'grid-table', '', this.container);
