48914b5739e6e744fe756c8a20b25c39967146fe
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-24 13:48:33 +0100

    Add updateGameState to work with to grids for 2 players

--- battleships_game/client/src/models/game.js
+++ battleships_game/client/src/models/game.js
@@ -19,60 +19,25 @@ Game.prototype.bindEvents = function () {
     this.render();
   });
   PubSub.subscribe('GridTileView:tile-clicked', (event) => {
-    this.attemptCounter += 1;
-    console.log("id passed to game", event.detail);
-    const tileIdString = event.detail;
-    // comes as a string though so need to convert
-    const tileRow = parseInt(tileIdString[0]);
-    const tileCol = parseInt(tileIdString[1]);
-    //check current state of corresponding tile
-    const currentState = this.gamestate[tileRow][tileCol];
-    // change state of tile accordingly
-    //this may need to change depending on coding for states
-    // i assume for now there are 4 which are:
-    // 0 = no boat - no attempted bombing
-    // 1 = boat - no attempted bombing
-    // 2 = no boat - attempted bombing [MISS]
-    // 3 = boat - attempted bombing [HIT]
-    if (currentState === 0) {
-      // 0 = no boat - no attempted bombing
-      // so change to 1 as now no boat [MISS]
-      // no change to counter as missed
-      this.attemptCounter += 1;
-      this.attemptsLeft -= 1;
-      this.gamestate[tileRow][tileCol] = 2;
-    } else if (currentState === 1) {
-      // 1 = boat - no attempted bombing
-      // so change to 3 as hit boat [HIT]
-      // and add to counter
-      this.gamestate[tileRow][tileCol] = 3;
-      this.hitCounter += 1;
-    } else if (currentState === 2) {
-      // 2 = no boat - attempted bombing [MISS]
-      // so no change as attempted same tile as previous MISS
-      // no change to counter as still a MISS
-    } else if (currentState === 3) {
-      // 3 = boat - attempted bombing [HIT]
-      // so no change as attempted same tile as previous HIT
-      // no change to counter as still a HIT
-    }
-    // now check counter is not over number of ships required
-    // dummy 1 used for now to check functionality
-    // intended to be 5 for MVP - if we use 5 ships of size 1
-    if (this.hitCounter < 5) {
-      // then continue to render game until enough hits
-      this.render(this.gamestate);
+    this.turn = event.detail.turn
+    const tileRow = parseInt(tileIdString[1]);
+    const tileCol = parseInt(tileIdString[2]);
+
+    if (this.turn === 1) {
+      //do stuff on gamestatePlayer1
+      // if the tile has been used already doesn't do anything, just when is 0 or 1
+      if (this.gamestatePlayer1[tileRow][tileCol] < 2) {
+        this.updateGameState(this.gamestatePlayer1, this.attemptCounterPlayer1, this.hitCounterPlayer1 );
+        this.turn = 2;
+      }
+
     } else {
-      // enough hits so deal with finish game
-      this.result();
+      if (this.gamestatePlayer2[tileRow][tileCol] < 2) {
+        this.updateGameState(this.gamestatePlayer2, this.attemptCounterPlayer2, this.hitCounterPlayer2 );
+        this.turn = 1;
+      }
     }
 
-    //could also check attemptCounter here is we want to end the game that way
-    if (this.attemptsLeft < 1) {
-      this.result()
-    }
-
-    //but think that was an extension
 
   });
 
@@ -86,6 +51,54 @@ Game.prototype.bindEvents = function () {
   });
 };
 
+Game.prototype.updateGameState = function (gamestate, attemptCounter, hitCounter) {
+  attemptCounter += 1;
+
+  const tileIdString = event.detail.info;
+  // comes as a string though so need to convert
+  //check current state of corresponding tile
+  const currentState = gamestate[tileRow][tileCol];
+  // change state of tile accordingly
+  //this may need to change depending on coding for states
+  // i assume for now there are 4 which are:
+  // 0 = no boat - no attempted bombing
+  // 1 = boat - no attempted bombing
+  // 2 = no boat - attempted bombing [MISS]
+  // 3 = boat - attempted bombing [HIT]
+  if (currentState === 0) {
+    // 0 = no boat - no attempted bombing
+    // so change to 1 as now no boat [MISS]
+    // no change to counter as missed
+    attemptCounter += 1;
+    gamestate[tileRow][tileCol] = 2;
+  } else if (currentState === 1) {
+    // 1 = boat - no attempted bombing
+    // so change to 3 as hit boat [HIT]
+    // and add to counter
+    gamestate[tileRow][tileCol] = 3;
+    hitCounter += 1;
+  }
+  // else if (currentState === 2) {
+    // 2 = no boat - attempted bombing [MISS]
+    // so no change as attempted same tile as previous MISS
+    // no change to counter as still a MISS
+  // } else if (currentState === 3) {
+    // 3 = boat - attempted bombing [HIT]
+    // so no change as attempted same tile as previous HIT
+    // no change to counter as still a HIT
+  // }
+  // now check counter is not over number of ships required
+  // dummy 1 used for now to check functionality
+  // intended to be 5 for MVP - if we use 5 ships of size 1
+  if (hitCounter < 5) {
+    // then continue to render game until enough hits
+    this.render(gamestate);
+  } else {
+    // enough hits so deal with finish game
+    this.result();
+  }
+
+};
 
 Game.prototype.handleNewGame = function () {
   PubSub.subscribe('ResultView:play-again', (event) => {
