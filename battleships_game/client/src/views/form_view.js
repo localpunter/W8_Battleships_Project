7916abf78f795fc1bca2fbc7525df90b67beb2fb
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-25 16:08:27 +0100

    Ships now dissapear when they have been already placed

--- battleships_game/client/src/views/form_view.js
+++ battleships_game/client/src/views/form_view.js
@@ -42,27 +42,45 @@ FormView.prototype.createSelects = function (form1, form2) {
 
   let selectShip;
   let selectPosition;
+  let currentShips
 
   if (this.gamestate.turn === 1) {
     selectShip = createAndAppend('select', `select-ship`, '', form1);
     selectPosition = createAndAppend('select', `select-position`, '', form1);
     reset = createAndAppend('button','reset', 'Reset', form1 );
+    currentShips = this.gamestate.shipArrayPlayer1;
   } else {
     selectShip = createAndAppend('select', `select-ship`, '', form2);
     selectPosition = createAndAppend('select', `select-position`, '', form2);
     reset = createAndAppend('button','reset', 'Reset', form2 );
+    currentShips = this.gamestate.shipArrayPlayer2;
   };
 
-  const destroyer = createAndAppend('option', 1 , 'Destroyer', selectShip);
-  destroyer.value = 1;
-  const cruiser = createAndAppend('option', 2, 'Cruiser', selectShip);
-  cruiser.value = 2;
-  const submarine = createAndAppend('option', 3, 'Submarine', selectShip);
-  submarine.value = 3;
-  const battleship = createAndAppend('option', 4, 'Battleship', selectShip);
-  battleship.value = 4;
-  const carrier = createAndAppend('option', 5, 'Carrier', selectShip);
-  carrier.value = 5;
+
+  if (currentShips[0] === 0) {
+    const destroyer = createAndAppend('option', 1 , 'Destroyer', selectShip);
+    destroyer.value = 1;
+  }
+
+  if (currentShips[1] === 0) {
+    const cruiser = createAndAppend('option', 2, 'Cruiser', selectShip);
+    cruiser.value = 2;
+  }
+
+  if (currentShips[2] === 0) {
+    const submarine = createAndAppend('option', 3, 'Submarine', selectShip);
+    submarine.value = 3;
+  }
+
+  if (currentShips[3] === 0) {
+    const battleship = createAndAppend('option', 4, 'Battleship', selectShip);
+    battleship.value = 4;
+  }
+
+  if (currentShips[4] === 0) {
+    const carrier = createAndAppend('option', 5, 'Carrier', selectShip);
+    carrier.value = 5;
+  }
 
   const horizontal = createAndAppend('option', 1, 'Horizonal', selectPosition );
   horizontal.value = 1;
