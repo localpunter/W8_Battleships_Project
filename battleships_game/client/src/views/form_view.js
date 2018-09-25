22d659f03cf5f8cac1b51526a3b8c11aeb2d6bcb
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-25 14:51:34 +0100

    Add select options for position and ships

--- battleships_game/client/src/views/form_view.js
+++ battleships_game/client/src/views/form_view.js
@@ -12,9 +12,12 @@ FormView.prototype.render = function (event) {
   const info = "place your ships here"
 
   const form1 = createAndAppend('div', 'form1', '' , this.container)
+  console.log(form1);
   createAndAppend('h3', 'game-info', `Player 1 ${info}` , form1)
   const form2 = createAndAppend('div', 'form2', '' , this.container)
   createAndAppend('h3', 'game-info', `Player 2 ${info}` , form2)
+
+  this.createSelects(form1, form2);
 
   const formTable1 = createAndAppend('table', 'form-table', '', form1)
   for (let i = 0; i < this.gamestate.player1.length; i++) {
@@ -33,6 +36,34 @@ FormView.prototype.render = function (event) {
       formTileView.bindEvents();
     };
   };
+
+
+};
+
+FormView.prototype.createSelects = function (form1, form2) {
+
+  let selectShip;
+  let selectPosition;
+
+  if (this.gamestate.turn === 1) {
+    selectShip = createAndAppend('select', `select-ship`, '', form1);
+    selectPosition = createAndAppend('select', `select-position`, '', form1);
+  } else {
+    selectShip = createAndAppend('select', `select-ship`, '', form2);
+    selectPosition = createAndAppend('select', `select-position`, '', form2);
+  }
+
+  const destroyer = createAndAppend('option', 1 , 'Destroyer', selectShip);
+  const cruiser = createAndAppend('option', 2, 'Cruiser', selectShip);
+  const submarine = createAndAppend('option', 3, 'Submarine', selectShip);
+  const battleship = createAndAppend('option', 4, 'Battleship', selectShip);
+  const carrier = createAndAppend('option', 5, 'Carrier', selectShip);
+
+  const horizontal = createAndAppend('option', 1, 'Horizonal', selectPosition );
+  const vertical = createAndAppend('option', 2, 'Vertical', selectPosition );
+
+
+
 };
 
 module.exports = FormView;
